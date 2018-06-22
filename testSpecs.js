9c80f64253a14ed5e01b0fcf6ad7754080ea4822
Irene Hakes <irenebrbr@gmail.com>
2018-06-22 12:08:17 -0400

    initial commit

---
+++ testSpecs.js
@@ -0,0 +1,7 @@
+describe("The 'welcome' funtion", function(){
+
+    it('returns Welcome to Foundations', function(){
+        expect(welcome()).toEqual('Welcome to Foundations!');
+    });
+    
+});
